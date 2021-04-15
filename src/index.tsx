c86d612b7bb20a81b9ea3f31cd7e9adc486212ad
YoshijiFujiwara <yfbiology2@gmail.com>
2021-04-16 05:41:58 +0900

    un pkg path plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from "esbuild-wasm";
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
   const ref = useRef<any>();
@@ -22,14 +23,14 @@ const App = () => {
       return;
     }
 
-    const result = await ref.current.transform(input, {
-      loader: "ts",
-      target: "esnext",
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
     });
 
-    console.log(result);
-
-    setCode(result.code);
+    setCode(result.outputFiles[0].text);
   };
 
   return (
