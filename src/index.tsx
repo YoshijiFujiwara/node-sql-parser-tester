6234aa75a29ce7588caa485e8c6eb8303a96f149
YoshijiFujiwara <yfbiology2@gmail.com>
2021-04-16 07:57:41 +0900

    add sql example

--- src/index.tsx
+++ src/index.tsx
@@ -10,7 +10,8 @@ const initialSQL = "SELECT id, name, created_at FROM users;";
 
 const App = () => {
   const ref = useRef<any>();
-  const [input, setInput] = useState(initialSQL);
+  const [input, setInput] = useState<string>(initialSQL);
+  const [isLoading, setLoading] = useState<boolean>(false);
 
   const startService = async () => {
     ref.current = await esbuild.startService({
@@ -26,8 +27,11 @@ const App = () => {
     if (!ref.current) {
       return;
     }
+    setLoading(true);
+
     const executionCode = await bundler(input, ref.current);
     eval(executionCode);
+    setLoading(false);
   };
 
   return (
@@ -40,6 +44,29 @@ const App = () => {
           </li>
           <li>エラー処理が甘いので、SQL文を間違うと、赤い画面が出そう</li>
           <li>ブラウザで動かすためにesbuildでバンドルしてeval()してます</li>
+          <li>なので、動きそうなJSとか書いちゃダメです。SQL文のみ頼みます</li>
+          <li>
+            SQL文章例
+            <ul>
+              <li>SELECT * FROM hoge WHERE id = 3;</li>
+              <li>INSERT INTO syain VALUES (1,'鈴木','suzuki');</li>
+              <li>
+                UPDATE dept SET dname = '技術部', loc = '福岡' WHERE deptno =
+                80;
+              </li>
+              <li>DELETE FROM dept WHERE deptno = 80;</li>
+              <li>
+                SELECT id_c, AVG(quantity), ( SELECT AVG(quantity) FROM
+                purchase) FROM purchase GROUP BY id_c;
+              </li>
+              <li>
+                SELECT id_p, fullname FROM purchase JOIN customer ON
+                purchase.id_c=customer.id_c;
+              </li>
+              <li>create table mybook ( id integer );</li>
+              <li>drop table users;</li>
+            </ul>
+          </li>
         </ul>
       </div>
       <CodeEditor
@@ -52,9 +79,11 @@ const App = () => {
         <button
           className="button is-rounded is-primary is-big"
           onClick={onClick}
+          disabled={isLoading}
         >
           astify
         </button>
+        {isLoading && <p> 実行中...</p>}
       </div>
       <pre id="preview"></pre>
     </div>
