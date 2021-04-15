c3d7ab25fd9f961f5a374b24ae975f3d544f8fd2
YoshijiFujiwara <yfbiology2@gmail.com>
2021-04-16 05:13:42 +0900

    initializing esbuild

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,21 @@
-import { useState } from "react";
+import * as esbuild from "esbuild-wasm";
+import { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
+
+  const startService = async () => {
+    const service = await esbuild.startService({
+      worker: true,
+      wasmURL: "/esbuild.wasm", // いったん、publicフォルダにおいた
+    });
+    console.log(service);
+  };
+  useEffect(() => {
+    startService();
+  }, []);
 
   const onClick = () => {
     console.log(input);
