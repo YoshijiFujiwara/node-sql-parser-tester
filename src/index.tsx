d1d7b61dd7e1d20b1855a8cb6ae68d5e9446826f
YoshijiFujiwara <yfbiology2@gmail.com>
2021-04-16 05:08:51 +0900

    add textarea

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,26 @@
-import ReactDOM from 'react-dom';
+import { useState } from "react";
+import ReactDOM from "react-dom";
 
 const App = () => {
-  return <h1>Hi</h1>;
-}
+  const [input, setInput] = useState("");
+  const [code, setCode] = useState("");
 
-ReactDOM.render(
-  <App/>,
-  document.querySelector('#root')
-)
+  const onClick = () => {
+    console.log(input);
+  };
+
+  return (
+    <div>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={onClick}>submit</button>
+      </div>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.querySelector("#root"));
